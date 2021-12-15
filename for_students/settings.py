92f0e8e277e95cbda7d9693b37fd5facfe34be53
amurad77 <amurad7799@gmail.com>
2021-12-15 05:53:56 -0800

    finish

--- for_students/settings.py
+++ for_students/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-s54@j00r1dx$p2f(0hnydg(q)e0$p#c5^5slvs1y$sjy1!wk_h
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
