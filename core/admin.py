0c3940513e5523248c140bf1dde1f429368f5b94
amurad77 <amurad7799@gmail.com>
2021-12-15 06:11:27 -0800

    finish

--- core/admin.py
+++ core/admin.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 
 # Register your models here.
 
-from core.models import University_left, University_right, Book, Project, Contact
+from core.models import University, Book, Project, Contact
 
-admin.site.register([University_left, University_right, Book, Project, Contact])
+admin.site.register([University, Book, Project, Contact])
