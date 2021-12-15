20c5763dd6fdd4326ed4bd90dd6a4b128ce568a3
amurad77 <amurad7799@gmail.com>
2021-12-15 12:23:06 -0800

    finish

--- for_students/settings.py
+++ for_students/settings.py
@@ -76,15 +76,19 @@ WSGI_APPLICATION = 'for_students.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'db_name',
-        'USER': 'db_user',
-        'PASSWORD': '12345',
-        'HOST': 'localhost',
-        'PORT': 5432
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': os.getenv('POSTGRES_DB', 'taskilled'),
+            'USER': os.getenv('POSTGRES_USER', 'taskilled'),
+            'PORT': os.getenv('POSTGRES_PORT', 5432),
+            'PASSWORD': os.getenv('POSTGRES_PASSWORD', '12345'),
+            'HOST': os.getenv('POSTGRES_HOST', '127.0.0.1'),
+        }
+    #     'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
     }
-}
 
 
 # Password validation
